fe760f7a975e74157126c279630cd76c844f8030
halan10 <64984593+halan10@users.noreply.github.com>
2023-05-09 14:38:18 -0700

    Create React project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import CardsWrapper from "./components/CardsWrapper";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CardsWrapper cardsNumber="1" />
+      <button onClick={() => window.location.reload()}>Reload Cards</button>
     </div>
   );
 }
